4bb4fe85a6f2af3419c914d1d8fb050feb2c294f
Lokesh Kumar sharma <1383lokesh@gmail.com>
2023-09-22 15:26:01 +0530

    changes in the app

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import  User
+from django.contrib.auth import  authenticate, login
 from home.models import *
+from django.contrib import messages
 
 
 # Create your views here.
@@ -9,6 +11,23 @@ def test(request):
     return render(request,"home/base.html")
 
 def loginpage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if not User.objects.filter(username = username).exists():
+             messages.error(request, "User  does not exist")
+            
+        user = authenticate(username = username, password= password)
+
+        if user is None:
+             messages.error(request, "Invalid credential")
+             return redirect('/login')
+        else:
+            login(request,user)
+            return redirect('/home')
+
+        
+             
     return render(request,"home/login.html")
 
 def signuppage(request):
@@ -17,6 +36,11 @@ def signuppage(request):
         Name = request.POST.get('first_name')
         username = request.POST.get('username')
         password = request.POST.get('password')
+        user =User.objects.filter(username=username)
+        if user.exists():
+            messages.info(request, "Username is already exists")
+            return redirect('/signup')
+
         user = User.objects.create(
             first_name=Name,
             username=username
@@ -26,3 +50,9 @@ def signuppage(request):
 
         return redirect('/login')
     return render(request,"home/signup.html")
+
+def homepage(request):
+    return render(request,'home/home.html')
+
+def upload_image(request):
+    return render(request,'home/upload.html')
